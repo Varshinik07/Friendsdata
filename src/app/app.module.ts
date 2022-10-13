71fab2d6ded5e3bd2791793f0c821dae2e1173cc
Varshini <varshuk01@gmail.com>
2022-10-13 19:25:15 +0530

    add frined list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { AddfriendComponent } from './addfriend/addfriend.component';
+import{RouterModule,Routes} from '@angular/router'
+const appRoutes:Routes=[
+  {
+    path:"",component: AddfriendComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddfriendComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
