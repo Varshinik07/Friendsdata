2b5195aa0a12408ac44a840c86f6bd863c0d4c5a
Varshini <varshuk01@gmail.com>
2022-10-13 19:49:25 +0530

    view friend data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddfriendComponent } from './addfriend/addfriend.component';
-import{RouterModule,Routes} from '@angular/router'
+import{RouterModule,Routes} from '@angular/router';
+import { ViewFriendDataComponent } from './view-friend-data/view-friend-data.component'
 const appRoutes:Routes=[
   {
     path:"",component: AddfriendComponent
+  },
+  {
+    path:"view friend",component: ViewFriendDataComponent
   }
 ]
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AddfriendComponent
+    AddfriendComponent,
+    ViewFriendDataComponent
   ],
   imports: [
     BrowserModule,
